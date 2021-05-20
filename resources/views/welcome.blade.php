6a6d5dbda06eaf273c55fa22f90b844ffc87ff00
n.test <testn960@gmail.com>
2021-05-20 07:04:01 +0000

    added button of signup in welcome

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -4,7 +4,11 @@
     <div class="center jumbotron">
         <div class="text-center">
             <h1>Cafe & Restaurant Diary</h1>
-            <h2>お気に入りのカフェやレストランの記録をしましょう。</h2>
+            {{--文字小さくしたい--}}
+            <h4>お気に入りのカフェやレストランの記録をしましょう。</h4>
+            {{-- ユーザ登録ページへのリンク --}}
+            {!! link_to_route('signup.get', 'ユーザー登録をする', [], ['class' => 'btn btn-lg btn-primary']) !!}
+            {{--ログインページへのリンク--}}
         </div>
     </div>
 @endsection
