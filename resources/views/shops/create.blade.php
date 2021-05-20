974a055641ce2cea8d1968ab595046c43be6b434
n.test <testn960@gmail.com>
2021-05-20 01:23:50 +0000

    added all neccessary forms in create.blade

--- resources/views/shops/create.blade.php
+++ resources/views/shops/create.blade.php
@@ -13,13 +13,25 @@
                 <div class="form-group">
                     {{--第一引数に Form::modelで指定されていたShopモデルのインスタンスである $shop のカラム（この場合 'catrgory' カラム）--}}
                     {{--第二引数にラベル名--}}
-                    {!! Form::label('catrgory', 'カテゴリー:') !!}
+                    {!! Form::label('catrgory', 'カテゴリー') !!}
                     {{--第一引数 $shop の 'category' カラムを指定。Form::text() は <input type="text"> を生成--}}
                     {{--第二引数はテキストボックスに入れておきたい固定の初期値（不要なのでnull）--}}
                     {{--第三引数はタグの属性情報を配列形式で指定--}}
                     {!! Form::text('category', null, ['class' => 'form-control']) !!}
+                    
+                    {{--店名のフォーム--}}
+                    {!! Form::label('shop_name', '店名') !!}
+                    {!! Form::text('shop_name', null, ['class' => 'form-control']) !!}
+                    
+                    {{--所在地のフォーム--}}
+                    {!! Form::label('palce', '所在地') !!}
+                    {!! Form::text('place', null, ['class' => 'form-control']) !!}
+                    
+                    {{--その他のフォーム--}}
+                    {!! Form::label('other', 'その他（感想やメニュー内容など）') !!}
+                    {!! Form::text('other', null, ['class' => 'form-control']) !!}
                 </div>
-
+                {{--ボタンのデザインはとりあえずこれ。後で調整--}}
                 {!! Form::submit('登録', ['class' => 'btn btn-primary']) !!}
             {{--フォームを終了--}}
             {!! Form::close() !!}
