d030650419b7bca161f7780f05b2e22e37e0a4d6
n.test <testn960@gmail.com>
2021-05-20 04:30:51 +0000

    amended the contents of web.php

--- routes/web.php
+++ routes/web.php
@@ -15,33 +15,10 @@
 //indexアクションは、 / にアクセスしたときと、 /shops にアクセスした両方で同じルーティングが設定されたことになる。
 Route::get('/', 'ShopsController@index');
 
-// create: 新規作成用のフォームページ
-Route::get('shops/create', 'ShopsController@create')->name('shops.create');
+//get,post,put,delete,edit,create,showの補助を集約
+Route::resource('shops', 'ShopsController');
 
 
-// お店情報の新規登録を処理（新規登録画面を表示するためのものではない）
-Route::post('shops', 'ShopsController@store');
-
-
-//あえてresourceは使わない：エラーが出たときにたどりやすいようにするため。
-// 登録店舗の個別詳細・編集ページ表示（これは編集ページになれる？）
-Route::get('shops/{id}', 'ShopsController@show');
-
-
-// お店情報の更新処理（編集画面を表示するためのものではない）
-Route::put('shops/{id}', 'ShopsController@update');
-
-// 登録店舗を削除
-Route::delete('shops/{id}', 'ShopsController@destroy');
-
-//以下は補助ページ　nameは「名前付きルート」：特定のルートへのURLを生成したり、リダイレクトしたりする場合に便利
-// index: showの補助ページ（一覧ページ）
-Route::get('shops', 'ShopsController@index')->name('shops.index');
-
-
-
-// edit: 更新用のフォームページ
-Route::get('shops/{id}/edit', 'ShopsController@edit')->name('shops.edit');
 
 
 
