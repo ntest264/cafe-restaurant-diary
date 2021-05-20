16ad1bca9014223b10ff9207160cd897fea156d8
n.test <testn960@gmail.com>
2021-05-20 07:25:43 +0000

    added @if things in welcome

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,11 +1,14 @@
 @extends('layouts.app')
 
 @section('content')
+  @if (Auth::check())
+        {{ Auth::user()->name }}
+  @else
     <div class="center jumbotron">
         <div class="text-center">
             <h1>Cafe & Restaurant Diary</h1>
             {{--文字小さくしたい--}}
-            <h4>お気に入りのカフェやレストランの記録をしましょう。</h4>
+            <h5>お気に入りのカフェやレストランの記録をしましょう。</h5>
             {{-- ユーザ登録ページへのリンク --}}
             {!! link_to_route('signup.get', 'ユーザー登録をする', [], ['class' => 'btn btn-lg btn-primary']) !!}
             {{--ログインページへのリンク--}}
