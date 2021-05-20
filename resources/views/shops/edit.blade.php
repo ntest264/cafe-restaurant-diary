fdce8ed21e0602ab4077159bb570e256518920c9
n.test <testn960@gmail.com>
2021-05-20 05:17:01 +0000

    added the contents of edit.blade

--- resources/views/shops/edit.blade.php
+++ resources/views/shops/edit.blade.php
@@ -2,6 +2,35 @@
 
 @section('content')
 
-<!-- ここにページ毎のコンテンツを書く -->
+<h1>登録番号 {{ $shop->id }} の編集ページ</h1>
+
+    <div class="row">
+        <div class="col-6">
+            {!! Form::model($shop, ['route' => ['shops.update', $shop->id], 'method' => 'put']) !!}
+
+                <div class="form-group">
+                    {{--カテゴリーのフォーム--}}
+                    {!! Form::label('catrgory', 'カテゴリー') !!}
+                    {!! Form::text('category', null, ['class' => 'form-control']) !!}
+                    
+                    {{--店名のフォーム--}}
+                    {!! Form::label('shop_name', '店名') !!}
+                    {!! Form::text('shop_name', null, ['class' => 'form-control']) !!}
+                    
+                    {{--所在地のフォーム--}}
+                    {!! Form::label('palce', '所在地') !!}
+                    {!! Form::text('place', null, ['class' => 'form-control']) !!}
+                    
+                    {{--その他のフォーム--}}
+                    {!! Form::label('other', 'その他（感想やメニュー内容など）') !!}
+                    {!! Form::text('other', null, ['class' => 'form-control']) !!}
+                </div>
+
+                {!! Form::submit('更新する', ['class' => 'btn btn-primary']) !!}
+
+            {!! Form::close() !!}
+        </div>
+    </div>
+
 
 @endsection
