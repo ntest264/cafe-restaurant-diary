be5668c3d532b398a0eb7ffa3f05352c940825b0
n.test <testn960@gmail.com>
2021-05-21 01:03:10 +0000

    include shops.blade into welcome.blade

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,13 +1,25 @@
 @extends('layouts.app')
 
 @section('content')
-  @if (Auth::check())
-        {{ Auth::user()->name }}
+   {{--以下@elseまでは仮で　ユーザー名表示の部分とかは削るかも--}}
+   @if (Auth::check())
+          <div class="row">
+            <aside class="col-sm-4">
+                <div class="card">
+                    <div class="card-header">
+                        <h3 class="card-title">{{ Auth::user()->name }}</h3>
+                    </div>
+                </div>
+            </aside>
+            <div class="col-sm-8">
+                {{-- 登録店舗一覧 --}}
+                @include('shops.shops')
+            </div>
+        </div>
   @else
     <div class="center jumbotron">
         <div class="text-center">
             <h1>Cafe & Restaurant Diary</h1>
-            {{--文字小さくしたい--}}
             <h5>お気に入りのカフェやレストランの記録をしましょう。</h5>
             {{-- ユーザ登録ページへのリンク --}}
             {!! link_to_route('signup.get', 'ユーザー登録をする', [], ['class' => 'btn btn-lg btn-primary']) !!}
