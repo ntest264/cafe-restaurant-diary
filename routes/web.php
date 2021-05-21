361a15db515a0613513d3779f0981ac1da49b470
n.test <testn960@gmail.com>
2021-05-21 00:41:33 +0000

    changed the contents of line 16 in web.php

--- routes/web.php
+++ routes/web.php
@@ -13,12 +13,8 @@
 
 //トップページ / をShopsControllerのindexアクションに設定。
 //indexアクションは、 / にアクセスしたときと、 /shops にアクセスした両方で同じルーティングが設定されたことになる。
-//Route::get('/', 'ShopsController@index');
-//以下を追加したため一時的にコメントアウト
+Route::get('/', 'ShopsController@index');
 
-Route::get('/', function () {
-    return view('welcome');
-});
 
 //get,post,put,delete,edit,create,showの補助を集約
 Route::resource('shops', 'ShopsController');
