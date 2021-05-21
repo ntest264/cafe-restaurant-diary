0a2cca43f2fcb0ce18a3d4d314953bee37ceff35
n.test <testn960@gmail.com>
2021-05-21 00:25:24 +0000

    added the contents of Shop.php

--- app/Shop.php
+++ app/Shop.php
@@ -6,5 +6,14 @@ use Illuminate\Database\Eloquent\Model;
 
 class Shop extends Model
 {
-    //
+    protected $fillable = ['content'];
+
+    /**
+     * この登録情報を所有するユーザ。（ Userモデルとの関係を定義）
+     */
+    //Shopを持つUserは一人なので単数で
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
 }
