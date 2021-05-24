4c816faeae0e06e78532e99a66eaf6b17e20183f
n.test <testn960@gmail.com>
2021-05-24 00:55:49 +0000

    changed the place of message from the bottom to the top

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -11,17 +11,19 @@
          {{-- ナビゲーションバー --}}
         @include('commons.navbar')
 
-        <div class="container">
-              {{-- エラーメッセージ --}}
-            @include('commons.error_messages')
-            @yield('content')
-        </div>
-        {{--フラッシュメッセージ--}}
+         {{--フラッシュメッセージ--}}
         @if (session('flash_message'))
           <div class="flash_message bg-success text-center py-3 my-0">
               {{ session ('flash_message') }}
           </div>
         @endif
+        
+        <div class="container">
+              {{-- エラーメッセージ --}}
+            @include('commons.error_messages')
+            @yield('content')
+        </div>
+        
 
         <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js"></script>
         <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.6/umd/popper.min.js"></script>
