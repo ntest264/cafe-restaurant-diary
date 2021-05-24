b9dcb06f915237f4aba3d1028bdc34f4181cb5e8
n.test <testn960@gmail.com>
2021-05-24 06:57:02 +0000

    added br in welcom.blade

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -17,8 +17,11 @@
         <div class="text-center">
             <h1>Cafe & Restaurant Diary</h1>
             <h5>お気に入りのカフェやレストランの記録をしましょう。</h5>
+            <br>
             {{-- ユーザ登録ページへのリンク --}}
             {!! link_to_route('signup.get', 'ユーザー登録をする', [], ['class' => 'btn btn-lg btn-primary']) !!}
+            <br>
+            <br>
             {{--ログインページへのリンク--}}
             {!! link_to_route('login.post', 'ログインする', [], ['class' => 'btn btn-lg btn-primary']) !!}
         </div>
